6fde4b1dac82fe6f6838b07669f2ebc4498ec2cb
Asad Rizvi <syedasadrazarizvi1@gmail.com>
2022-05-09 17:37:24 +1000

    Lets go

--- bot.py
+++ bot.py
@@ -2,93 +2,189 @@ import os
 
 import asyncio
 import discord
+import youtube_dl
+import validators
 from discord import FFmpegPCMAudio
 from discord.ext import commands
 from discord.voice_client import VoiceClient
 from dotenv import load_dotenv
 from random import randint
 
+# GLOBAL VARIABLES--------------------------------------------------------------
 VOICE_CHANNEL = 'ball-channel'
 WORK_CHANNEL = 'bot-tests'
 ASAD = 'Daruni#8443'
 cat_gif = 'https://tenor.com/view/good-night-flynn-flynn-park-flynn-gif-24987363'
+RANDOM_GIF = 'https://tenor.com/view/crying-sobbing-uncontrollably-sad-cry-gif-19906768'
+data = {"guilds": []}
+song_queue = []
+tasker = None
 
+# ENVIRONMENT VARIABLES---------------------------------------------------------
 load_dotenv()
 TOKEN = os.getenv('DISCORDTOKEN')
 GUILD = os.getenv('DISCORDSERVER')
-# print(SERVER)
 
-client = discord.Client()
+# client = discord.Client()
 
-data = {"guilds": []}
-'''
-data = {
-    "guilds": [{"g_name": f, "channel": channel}]
+ytdl_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '192.168.0.5'
 }
-'''
+
+ytdl = youtube_dl.YoutubeDL(ytdl_options)
+
+
+ffmpeg_options = {
+    'options': '-vn'
+}
 
 bot = commands.Bot(command_prefix='!')
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.9):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.duration = data.get('duration')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False, play=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(f"ytsearch:{url}", download=not stream or play))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 @bot.event
 async def on_ready():
     print("Launching Bot")
-    await asyncio.sleep(7)
-    '''
-    print(bot.voice_clients)
-    for vc in bot.voice_clients:
-        print(f'Channel is {vc.channel}')
-        print(len(vc.channel.members))
-        if len(vc.channel.members) < 1:
-            print('Disconnecting Bot')
-            return await vc.disconnect()
-    '''
 
 
-@client.event
-async def on_message(message):
-    if message.author == bot.user:
+@bot.command(name='play', aliases=['p', 'play_song'], help='Play or add song to queue')
+async def play(ctx, url: str, *args):
+    global song_queue
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    if not validators.url(url):
+        # url = ' '.join(args[0:])
+        to_play = url + " " + ' '.join(args[0:])
+        print(to_play)
+    url = to_play
+    try:
+        # If author is in vc join that channel, otherwise error
+        if(voice == None):
+            if not ctx.message.author.voice:
+                await ctx.send(f"{ctx.author} is not connected to a voice channel")
+            else:
+                await ctx.author.voice.channel.connect()
+
+        elif voice.channel != ctx.author.voice.channel:
+            await ctx.send('I am already in another channel!')
+
+        async with ctx.typing():
+            # The bot's connection to a channel
+            voice_client = ctx.message.guild.voice_client
+            '''
+            if not (voice_client.is_playing() and voice_client.is_paused()):
+                song_queue.clear()
+            '''
+            player = await YTDLSource.from_url(url, loop=bot.loop, stream=True)
+            if len(song_queue) == 0:
+                await start_playing(ctx, player)
+            else:
+                song_queue.append(player)
+                await ctx.send(f"**Queued at position {len(song_queue)-1}:** {player.title}")
+    except Exception as error:
+        print(f'An error has occured: {error}')
+
+
+async def start_playing(ctx, player):
+    global song_queue
+    song_queue.append(player)
+    if (song_queue[0] == None):
         return
-    '''
-    print(f'{message.author} posted in {message.channel}')
-    if str(message.author) != ASAD:
-        print("not asad")
-        return
-    '''
-    if str(message.channel) != WORK_CHANNEL:
-        print("not in tests, ignoring")
-        return
+    i = 0
+    while i < len(song_queue):
+        try:
+            # ctx.voice_client.play(song_queue[0], after=lambda e: print('Player error: %s' % e) if e else None)
+            ctx.voice_client.play(song_queue[0], after=lambda e: print(
+                f'Player error: {e}') if e else None)
+            await ctx.send(f"**Now playing:** {song_queue[0].title}")
+        except Exception as e:
+            await ctx.send(f"Something went wrong: {e}")
+        # await asyncio.sleep(song_queue[0].duration)
+        global tasker
+        tasker = asyncio.create_task(coro(ctx, song_queue[0].duration))
+        try:
+            await tasker
+        except asyncio.CancelledError:
+            print("Task cancelled")
+        if(len(song_queue) > 0):
+            song_queue.pop(0)
 
-    print(f"New message from: {message.author}")
-    # If the message was posted in the bot-tests channel
-    if str(message.channel) == WORK_CHANNEL:
-        await message.channel.send(message.content)
-    if 'flynn' in str(message.content).lower():
-        await message.channel.send(cat_gif)
+
+async def coro(ctx, duration):
+    # Reconsider context, not really needed?
+    await asyncio.sleep(duration)
 
 
-@bot.command()
-async def play(ctx, *args):
-    to_play = ' '.join(args[0:])
-    channel = ctx.author.voice.channel
-    await channel.connect()
+@bot.command(name='queue', aliases=['q', 'list'], help='This command displays the queue')
+async def queued(ctx):
+    global song_queue
+    a = ""
+    i = 0
+    for f in song_queue:
+        if i > 0:
+            a = a + str(i) + ". " + f.title + "\n "
+        i += 1
+    await ctx.send("Queued songs: \n " + a)
 
-    '''
-    author = ctx.message.author
-    for member in discord.VoiceChannel:
-        if member == author:
-            print(member) 
 
-    channel = author.voice_channel
-    # await channel.connect()
-    await VoiceChannel.connect(channel)
-    
-    if to_play == 'word':
-        source = FFmpegPCMAudio(
-            "https://www.youtube.com/watch?v=_YzD9KW82sk", executable="ffmpeg")
-        ctx.voice_client.play(source, after=None)
-    '''
-    await ctx.send(f'Cannot play "{to_play}"')
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await ctx.send("Paused playing.")
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await ctx.send("Resumed playing.")
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+
+@bot.command(name='skip', help='Skip the song')
+async def skip(ctx):
+    global tasker
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.stop()
+        tasker.cancel()
+        await ctx.send("Skipped song.")
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
 
 
 @bot.command()
@@ -98,8 +194,11 @@ async def flynn(ctx):
 
 @bot.command()
 async def flip(ctx):
-    outcome = randint(1, 2)
-    await ctx.send("Heads") if outcome == 1 else await ctx.send("Tails")
+    outcome = randint(0, 100)
+    if outcome == 74:
+        await ctx.send(RANDOM_GIF)
+    else:
+        await ctx.send(f"Heads {str(outcome)}") if outcome % 2 == 1 else await ctx.send(f"Tails {str(outcome)}")
 
 
 @bot.command()
@@ -139,13 +238,18 @@ async def disconnect(ctx):
     for vc in bot.voice_clients:
         if vc.guild == ctx.message.guild:
             return await vc.disconnect()
+    clear()
     return await ctx.send("Bot is not in any voice chat currently.")
 
 
 @bot.command()
 async def clear(ctx):
     global data
+    global tasker
+    global song_queue
     data = {"guilds": []}
+    tasker = None
+    song_queue = []
     return
 
 
@@ -170,4 +274,39 @@ async def on_ready():
     print(f'The working channel is: {channel.name}')
     global data
     data['guilds'].append(dict(g_name=guild.name, channel=channel.name))
+'''
+'''
+@client.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    print(f'{message.author} posted in {message.channel}')
+    if str(message.author) != ASAD:
+        print("not asad")
+        return
+    
+    if str(message.channel) != WORK_CHANNEL:
+        print("not in tests, ignoring")
+        return
+
+    print(f"New message from: {message.author}")
+    # If the message was posted in the bot-tests channel
+    if str(message.channel) == WORK_CHANNEL:
+        await message.channel.send(message.content)
+    if 'flynn' in str(message.content).lower():
+        await message.channel.send(cat_gif)
+'''
+'''
+data = {
+    "guilds": [{"g_name": f, "channel": channel}]
+}
+'''
+'''
+@bot.command()
+async def typing(ctx, help='type for 10 seconds'):
+    channel = ctx.channel
+    async with channel.typing():
+        await asyncio.sleep(10)  # Wait 10 seconds with typing status
+    await channel.send(f'I just {help}')
 '''
