560214f640089451b5153b5d3e0683bfb3b6685c
SeedRizvi <105142489+SeedRizvi@users.noreply.github.com>
2022-08-11 00:25:56 +1000

    Experimental Reward System changes

--- items.py
+++ items.py
@@ -8,10 +8,10 @@ def get_items():
     items.append(dict(name="The YBS Rat", value=1000000))
     # Adding Ultra Rares
     items.append(dict(name="Reuben's Plushie", value=100000))
-    items.append(dict(name="Flynn's Grinder", value=100000))
+
     items.append(dict(name="Shower Cereal", value=100000))
     items.append(dict(name="Fresh Microphone", value=100000))
-    items.append(dict(name="Asad's Groutfit", value=100000))
+    items.append(dict(name="Asad's Outfit", value=100000))
 
     # Adding Rares
     items.append(dict(name="Frank's Webcam", value=20000))
